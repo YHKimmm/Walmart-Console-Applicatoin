b6540121bf64e7e697783e4af3bae2401d0dc3ff
YHKimmm <102700164+YHKimmm@users.noreply.github.com>
2022-10-26 20:59:35 -0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.MainMenu();
+        }
+    }
+}
